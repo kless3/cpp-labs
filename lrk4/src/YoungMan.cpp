8cba7d737c02755604dc62493fc0710bbdadc448
kless <maksefimchick@gmail.com>
2025-09-19 08:27:02 +0300

    Update directory tree

--- lrk4/src/YoungMan.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/YoungMan.h"
-#include <iostream>
-
-YoungMan::YoungMan(const std::string& n) : Person(n) {}
-
-void YoungMan::reactToNewPerson(const Person* other) const {
-    std::cout << name << " (young man): ";
-    std::cout << "Hello! Nice to meet you" << std::endl;
-}
