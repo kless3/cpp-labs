3157652675a60a8d56879048a6b5cbd1fbdd2a71
kless <maksefimchick@gmail.com>
2025-09-21 14:18:16 +0300

    Add final fixes pls

--- laba4/src/main.cpp
+++ laba4/src/main.cpp
@@ -9,8 +9,17 @@ const int EXIT_OPTION = 6;
 
 using namespace std;
 
+// Прототипы функций
+void showAllAreas(Shape** shapes, int shapeCount);
+void addRectangle(Shape*** shapes, int* shapeCount, int* capacity);
+void addCircle(Shape*** shapes, int* shapeCount, int* capacity);
+void addRightTriangle(Shape*** shapes, int* shapeCount, int* capacity);
+void addTrapezoid(Shape*** shapes, int* shapeCount, int* capacity);
+void cleanupMemory(Shape** shapes, int shapeCount);
+void resizeArray(Shape*** shapes, int shapeCount, int* capacity);
+
 int main() {
-    Shape** shapes = nullptr;  // Указатель на массив указателей
+    Shape** shapes = nullptr;
     int shapeCount = 0;
     int capacity = 0;
 
@@ -27,125 +36,102 @@ int main() {
         cin >> choice;
 
         switch (choice) {
-            case 1: {
-                cout << "\nAreas of all shapes:" << endl;
-                for (int i = 0; i < shapeCount; ++i) {
-                    cout << "Shape " << i + 1 << ": " << shapes[i]->area() << endl;
-                }
-                break;
-            }
-
-            case 2: {
-                double w;
-                double h;
-                cout << "Enter width and height: ";
-                cin >> w >> h;
-
-                // Увеличиваем емкость массива при необходимости
-                if (shapeCount >= capacity) {
-                    int newCapacity = (capacity == 0) ? 10 : capacity * 2;
-                    Shape** newShapes = new Shape*[newCapacity];
-
-                    // Копируем существующие указатели
-                    for (int i = 0; i < shapeCount; ++i) {
-                        newShapes[i] = shapes[i];
-                    }
-
-                    // Освобождаем старый массив
-                    delete[] shapes;
-                    shapes = newShapes;
-                    capacity = newCapacity;
-                }
-
-                shapes[shapeCount++] = new Rectangle(w, h);
-                break;
-            }
-
-            case 3: {
-                double r;
-                cout << "Enter radius: ";
-                cin >> r;
-
-                if (shapeCount >= capacity) {
-                    int newCapacity = (capacity == 0) ? 10 : capacity * 2;
-                    Shape** newShapes = new Shape*[newCapacity];
-
-                    for (int i = 0; i < shapeCount; ++i) {
-                        newShapes[i] = shapes[i];
-                    }
-
-                    delete[] shapes;
-                    shapes = newShapes;
-                    capacity = newCapacity;
-                }
-
-                shapes[shapeCount++] = new Circle(r);
-                break;
-            }
-
-            case 4: {
-                double b;
-                double h;
-                cout << "Enter base and height: ";
-                cin >> b >> h;
-
-                if (shapeCount >= capacity) {
-                    int newCapacity = (capacity == 0) ? 10 : capacity * 2;
-                    Shape** newShapes = new Shape*[newCapacity];
-
-                    for (int i = 0; i < shapeCount; ++i) {
-                        newShapes[i] = shapes[i];
-                    }
-
-                    delete[] shapes;
-                    shapes = newShapes;
-                    capacity = newCapacity;
-                }
-
-                shapes[shapeCount++] = new RightTriangle(b, h);
-                break;
-            }
-
-            case 5: {
-                double a;
-                double b;
-                double h;
-                cout << "Enter base1, base2 and height: ";
-                cin >> a >> b >> h;
-
-                if (shapeCount >= capacity) {
-                    int newCapacity = (capacity == 0) ? 10 : capacity * 2;
-                    Shape** newShapes = new Shape*[newCapacity];
-
-                    for (int i = 0; i < shapeCount; ++i) {
-                        newShapes[i] = shapes[i];
-                    }
-
-                    delete[] shapes;
-                    shapes = newShapes;
-                    capacity = newCapacity;
-                }
-
-                shapes[shapeCount++] = new Trapezoid(a, b, h);
-                break;
-            }
-
-            case 6: {
-                // Освобождаем память перед выходом
-                for (int i = 0; i < shapeCount; ++i) {
-                    delete shapes[i];
-                }
-                delete[] shapes;
-                cout << "Exiting program..." << endl;
-                break;
-            }
-
-            default: {
-                cout << "Invalid choice!" << endl;
-                break;
-            }
+            case 1: showAllAreas(shapes, shapeCount); break;
+            case 2: addRectangle(&shapes, &shapeCount, &capacity); break;
+            case 3: addCircle(&shapes, &shapeCount, &capacity); break;
+            case 4: addRightTriangle(&shapes, &shapeCount, &capacity); break;
+            case 5: addTrapezoid(&shapes, &shapeCount, &capacity); break;
+            case 6: cleanupMemory(shapes, shapeCount); break;
+            default: cout << "Invalid choice!" << endl; break;
         }
     } while (choice != EXIT_OPTION);
 
     return 0;
+}
+
+// Функция для изменения размера массива
+void resizeArray(Shape*** shapes, int shapeCount, int* capacity) {
+    int newCapacity = (*capacity == 0) ? 10 : *capacity * 2;
+    Shape** newShapes = new Shape*[newCapacity];
+
+    for (int i = 0; i < shapeCount; ++i) {
+        newShapes[i] = (*shapes)[i];
+    }
+
+    delete[] *shapes;
+    *shapes = newShapes;
+    *capacity = newCapacity;
+}
+
+// Функция для отображения всех площадей
+void showAllAreas(Shape** shapes, int shapeCount) {
+    cout << "\nAreas of all shapes:" << endl;
+    for (int i = 0; i < shapeCount; ++i) {
+        cout << "Shape " << i + 1 << ": " << shapes[i]->area() << endl;
+    }
+}
+
+// Функция для добавления прямоугольника
+void addRectangle(Shape*** shapes, int* shapeCount, int* capacity) {
+    double w;
+    double h;
+    cout << "Enter width and height: ";
+    cin >> w >> h;
+
+    if (*shapeCount >= *capacity) {
+        resizeArray(shapes, *shapeCount, capacity);
+    }
+
+    (*shapes)[(*shapeCount)++] = new Rectangle(w, h);
+}
+
+// Функция для добавления круга
+void addCircle(Shape*** shapes, int* shapeCount, int* capacity) {
+    double r;
+    cout << "Enter radius: ";
+    cin >> r;
+
+    if (*shapeCount >= *capacity) {
+        resizeArray(shapes, *shapeCount, capacity);
+    }
+
+    (*shapes)[(*shapeCount)++] = new Circle(r);
+}
+
+// Функция для добавления прямоугольного треугольника
+void addRightTriangle(Shape*** shapes, int* shapeCount, int* capacity) {
+    double b;
+    double h;
+    cout << "Enter base and height: ";
+    cin >> b >> h;
+
+    if (*shapeCount >= *capacity) {
+        resizeArray(shapes, *shapeCount, capacity);
+    }
+
+    (*shapes)[(*shapeCount)++] = new RightTriangle(b, h);
+}
+
+// Функция для добавления трапеции
+void addTrapezoid(Shape*** shapes, int* shapeCount, int* capacity) {
+    double a;
+    double b;
+    double h;
+    cout << "Enter base1, base2 and height: ";
+    cin >> a >> b >> h;
+
+    if (*shapeCount >= *capacity) {
+        resizeArray(shapes, *shapeCount, capacity);
+    }
+
+    (*shapes)[(*shapeCount)++] = new Trapezoid(a, b, h);
+}
+
+// Функция для очистки памяти
+void cleanupMemory(Shape** shapes, int shapeCount) {
+    for (int i = 0; i < shapeCount; ++i) {
+        delete shapes[i];
+    }
+    delete[] shapes;
+    cout << "Exiting program..." << endl;
 }
