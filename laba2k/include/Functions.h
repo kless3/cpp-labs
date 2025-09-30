7931769513e779ecdc3ab07f405ef49b71b361dd
kless <maksefimchick@gmail.com>
2025-09-30 12:19:10 +0300

    Add lrk2

---
+++ laba2k/include/Functions.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Matrix.h"
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void clearInputBuffer();
+
+void addMatrices(Matrix &matrix1, Matrix &matrix2, Matrix &result);
+
+void showResult(const Matrix &result);
+
+#endif
