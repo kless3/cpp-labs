b6354be560c857f656e770c4b54a4db92a3c4e3e
kless <maksefimchick@gmail.com>
2025-10-01 09:05:48 +0300

    Update directories

---
+++ laba7/include/Menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+
+void displayMainMenu();
+
+void readEmployeesFromFile();
+
+void inputEmployeesToFile();
+
+void clearEmployeesFile();
+
+#endif
