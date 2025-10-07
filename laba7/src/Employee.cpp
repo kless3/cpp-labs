17bb32b76c4e4fccc630c109dc992d6341f73ae9
kless <maksefimchick@gmail.com>
2025-10-07 12:02:49 +0300

    Update directories

--- laba7/src/Employee.cpp
+++
@@ -1,8 +0,0 @@
-#include <utility>
-
-#include "../include/Employee.h"
-
-Employee::Employee() : employeeNumber(0) {}
-
-Employee::Employee(std::string first, std::string middle, std::string last, int number)
-        : firstName(std::move(first)), middleName(std::move(middle)), lastName(std::move(last)), employeeNumber(number) {}
