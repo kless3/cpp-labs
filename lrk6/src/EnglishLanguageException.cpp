7255b8ae6b3c4d7dbbc19228a00f571f47eef2e8
kless <maksefimchick@gmail.com>
2025-10-30 01:12:04 +0300

    Update directories

--- lrk6/src/EnglishLanguageException.cpp
+++
@@ -1,4 +0,0 @@
-#include "../include/EnglishLanguageException.h"
-
-EnglishLanguageException::EnglishLanguageException(const std::string& text)
-        : std::invalid_argument("Текст содержит не русские символы: " + text) {}
