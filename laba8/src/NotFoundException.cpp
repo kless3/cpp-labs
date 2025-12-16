7ab2a6b295b4092f3aa3a050d2fb71b8e8e8a53d
kless <maksefimchick@gmail.com>
2025-12-16 20:36:53 +0300

    Update directories

--- laba8/src/NotFoundException.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Maksim on 04.11.2025.
-//
-
-#include "../include/NotFoundException.h"
-
-NotFoundException::NotFoundException(const std::string& message)
-        : std::runtime_error(message) {}
